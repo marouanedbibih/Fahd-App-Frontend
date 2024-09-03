c9e19fddb71865a7f4190a24b9760c0594f95fb2
Marouane Dbibih <marouane.dbibih@outlook.com>
2024-09-03 13:32:24 +0100

    Develop the departement interface

---
+++ src/components/Sidebar/SideBar.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  UserCircleIcon,
+  IdentificationIcon,
+  BuildingOfficeIcon,
+  CalendarIcon,
+} from "@heroicons/react/24/solid";
+
+interface SideBarProps {}
+
+const menuItems = [
+  {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: PresentationChartBarIcon,
+  },
+  {
+    title: "Employee",
+    path: "/employee",
+    icon: IdentificationIcon,
+  },
+  {
+    title: "Secretary",
+    path: "/secretary",
+    icon: UserCircleIcon,
+  },
+  {
+    title: "Department",
+    path: "/department",
+    icon: BuildingOfficeIcon,
+  },
+  {
+    title: "Schedule",
+    path: "/staff",
+    icon: CalendarIcon,
+  },
+];
+
+export const SideBar: React.FC<SideBarProps> = () => {
+  const pathname = usePathname() || "";
+  const router = useRouter();
+
+  return (
+    <Card
+      className="fixed h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl bg-white"
+      placeholder={undefined}
+      onPointerEnterCapture={undefined}
+      onPointerLeaveCapture={undefined}
+    >
+      <div className="mb-2 p-4">
+        <Typography
+          variant="h5"
+          color="blue-gray"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          Fahd App
+        </Typography>
+      </div>
+      <List
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
+        {menuItems.map((item) => (
+          <ListItem
+            key={item.title}
+            className={`hover:bg-blue-gray-50 cursor-pointer ${
+              pathname.startsWith(item.path)
+                ? "bg-blue-gray-50 font-semibold"
+                : ""
+            }`}
+            onClick={() => router.push(item.path)}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
+          >
+            <ListItemPrefix
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <item.icon
+                className={`h-5 w-5 ${
+                  pathname.startsWith(item.path)
+                    ? "text-blue-gray-900"
+                    : "text-blue-gray-700"
+                }`}
+              />
+            </ListItemPrefix>
+            {item.title}
+          </ListItem>
+        ))}
+      </List>
+    </Card>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+module.exports = withMT({
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'science-blue': {
+          50: '#F1F7FE',
+          100: '#E3ECFB',
+          200: '#C0D9F7',
+          300: '#888BF1',
+          400: '#4998E7',
+          500: '#227BD5',
+          600: '#1565C0',
+          700: '#114C93',
+          800: '#153965',
+          900: '#0E2443',
+          950: '#153965', // Ensure consistency for color value
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+});
